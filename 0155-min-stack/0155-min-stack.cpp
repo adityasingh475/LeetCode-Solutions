f18b9d964c44ce41635e0c5d95222d7736e34050
adityasingh475 <adityapratapsingh581@gmail.com>
2026-07-21 23:38:27 +0530

    Time: 35 ms (81.7%), Space: 151.1 MB (64.47%) - LeetHub - Time: O(1), Space: O(n)

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,38 @@
+#include <stack>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+class MinStack {
+private:
+    stack<pair<int, int>> st;
+
+public:
+    MinStack() {
+    }
+
+    void push(int val) {
+        if (st.empty()) {
+            st.push({val, val});
+        } else {
+            st.push({val, min(val, st.top().second)});
+        }
+    }
+
+    void pop() {
+        st.pop();
+    }
+
+    int top() {
+        return st.top().first;
+    }
+
+    int getMin() {
+        return st.top().second;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
